808e520d37cd68fcfde739ed12c4c9e3d54bab15
kartik01112004 <kartikgoel018@gmail.com>
2023-08-25 14:47:34 +0530

    initial

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,18 @@
 //imports
-const {ethers} = require("hardhat")
+const { ethers } = require("hardhat")
 
 async function main() {
-  const SimpleStorageFactory = await ethers.getContractFactory(
-    "SimpleStorage"
-  )
-  console.log("Deploying contract...")
-  const simpleStorage = await SimpleStorageFactory.deploy()
-
+    const SimpleStorageFactory =
+        await ethers.getContractFactory("SimpleStorage")
+    console.log("Deploying contract...")
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    await simpleStorage.deployed()
+    console.log(`Deployed conract to : ${simpleStorage.address}`)
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
