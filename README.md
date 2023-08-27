32eadc988bf264d1bf7e2819348026e96db84e77
kartik01112004 <kartikgoel018@gmail.com>
2023-08-27 23:12:42 +0530

    deploy on sepolia and verify

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Sample Hardhat Project
 
+yarn hardhat run scripts/deploy.js --network hardhat
+
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
 
 Try running some of the following tasks:
@@ -10,4 +12,10 @@ npx hardhat test
 REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.js
+yarn hardhat run scripts/deploy.js --network hardhat
+# to run on a perticular network here hardhat!
+
+yarn hardhat verify --network mainnet DEPLOYED_CONTRACT_ADDRESS "Constructor argument 1"
+# to deploy a verified contract from the terminal
+
 ```
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,44 @@
 //imports
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 async function main() {
     const SimpleStorageFactory =
         await ethers.getContractFactory("SimpleStorage")
     console.log("Deploying contract...")
     const simpleStorage = await SimpleStorageFactory.deploy()
-    await simpleStorage.deployed()
-    console.log(`Deployed conract to : ${simpleStorage.address}`)
+    // await simpleStorage.deployed()
+    console.log(`Deployed conract to : ${simpleStorage.target}`)
+    //hardhat gives RPC url and private key by itself
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for block txes...")
+        await simpleStorage.deploymentTransaction().wait(6) //error
+        await verify(simpleStorage.target, [])
+    }
+
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current value is ${currentValue}`)
+
+    //updating currentVlue
+    const transactionResponse = await simpleStorage.store(7)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated value is ${updatedValue}`)
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying the contract....")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verfied")) {
+            console.log("Already verified!")
+        } else {
+            console.log(e)
+        }
+    }
 }
 
 main()
